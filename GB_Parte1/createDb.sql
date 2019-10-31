721328ab86d33d9e2e299716def1eb360e20e5eb
elvisfb <elvisfb@edu.unisinos.br>
2019-10-31 00:57:21 -0300

    ajustado algumas coisas nas criações das tabelas e adicionado mais inserts

--- GB_Parte1/createDb.sql
+++ GB_Parte1/createDb.sql
@@ -4,62 +4,84 @@ USE MeLeva;
 
 drop table if exists Condutor;
 create table Condutor(
-    nome_cond varchar(255) not null,
-    telefone_cond varchar(9) not null,
     cpf_motorista varchar(11) primary key,
-    data_cadastra TIMESTAMP not null,
-    CHECK(CHAR_LENGTH(cpf_motorista) = 11)
+    nome_cond varchar(255) not null,
+    telefone_cond varchar(9) not null UNIQUE,
+    data_cadastro_cond TIMESTAMP not null,
+    CHECK(CHAR_LENGTH(cpf_motorista) = 11),
+    CHECK(CHAR_LENGTH(telefone_cond) = 9)
 );
+SELECT 'Criou Condutor' as '';
 
 drop table if exists veiculo;
 create table veiculo(
-    renavam integer PRIMARY KEY,
-    placa varchar(7) not null,
+    renavam varchar(9) PRIMARY KEY,
+    placa varchar(7) not null UNIQUE,
     marca varchar(255) not null,
     modelo varchar(255) not null,
-    ano integer,
-    CHECK(CHAR_LENGTH(placa) = 7)
+    ano int(4),
+    CHECK(CHAR_LENGTH(placa) = 7),
+    CHECK(CHAR_LENGTH(renavam) = 9),
+    CHECK(ano >= 1900)
 );
+SELECT 'Criou veiculo' as '';
 
 drop table if exists motorista;
 create table motorista(
-    data_inicio timestamp not null,
-    data_fim timestamp not null, 
-    renavam INTEGER not null,
+    data_inicio_mot timestamp not null,
+    data_fim_mot timestamp not null, 
+    renavam varchar(9),
     cpf_motorista varchar(11) not null,
-    id_motorista  integer PRIMARY KEY auto_increment,
-    UNIQUE(renavam, cpf_motorista, data_inicio, data_fim),
-    FOREIGN KEY (cpf_motorista) REFERENCES Condutor(cpf_motorista),
-    FOREIGN KEY (renavam) REFERENCES veiculo(renavam),
-    CHECK(CHAR_LENGTH(cpf_motorista) = 11)
+    id_motorista  int PRIMARY KEY auto_increment,
+    CONSTRAINT DIRIGE FOREIGN KEY (cpf_motorista) REFERENCES Condutor(cpf_motorista) on update cascade,
+    CONSTRAINT ALUGADO FOREIGN KEY (renavam) REFERENCES veiculo(renavam) on update cascade,
+    UNIQUE(renavam, cpf_motorista, data_inicio_mot, data_fim_mot),
+    check(DATEDIFF(data_inicio_mot,data_fim_mot) >= 0)
+    
 );
+SELECT 'Criou motorista' as '';
 
 drop table if exists passageiros;
 create table passageiros(
-    nome_cond varchar(255),
-    telefone_cond varchar(9) not null,
-    data_cadastro timestamp not null,
+    nome_pass varchar(255) not null,
+    telefone_pass varchar(9) not null UNIQUE,
+    data_cadastro_pass timestamp not null,
     cpf_passageiro varchar(11) PRIMARY KEY,
-    CHECK(CHAR_LENGTH(cpf_passageiro) = 11)
+    CHECK(CHAR_LENGTH(telefone_pass) = 9)
+    
 );
+SELECT 'Criou passageiros' as '';
 
 drop table if exists corrida;
 create table corrida(
     id_corrida integer auto_increment,
     avaliacao_condutor INT UNSIGNED,
     avaliacao_veiculo INT UNSIGNED,
-    data_inicio TIMESTAMP,
-    data_fim TIMESTAMP,
+    data_inicio_corr TIMESTAMP,
+    data_fim_corr TIMESTAMP,
     origem varchar(255),
     destino varchar(255),
     tarifa real,
-    distancia real,
+    distancia real UNSIGNED,
     id_motorista integer,
     cpf_passageiro varchar(11),
     PRIMARY KEY(id_corrida, id_motorista , cpf_passageiro),
-    FOREIGN KEY (id_motorista) REFERENCES motorista(id_motorista),
-    FOREIGN KEY (cpf_passageiro) REFERENCES passageiros(cpf_passageiro),
-    CHECK(CHAR_LENGTH(cpf_passageiro) = 11),
-    check( avaliacao_condutor <= 5),
-    check( avaliacao_veiculo <= 5)
+    CONSTRAINT ACEITA FOREIGN KEY (id_motorista) REFERENCES motorista(id_motorista) on update cascade,
+    CONSTRAINT SOLICITA FOREIGN KEY (cpf_passageiro) REFERENCES passageiros(cpf_passageiro) on update cascade,
+    CHECK( avaliacao_condutor <= 5),
+    CHECK( avaliacao_veiculo <= 5),
+    check(DATEDIFF(data_inicio_corr, data_fim_corr) >= 0)
+    
 );
+SELECT 'Criou corrida' as '';
+
+
+/*CHECK(data_cadastro_cond <= now()) fazer numa trigger */
+
+/*CHECK(data_inicio <= now()),
+    CHECK(data_fim <= now()) trigger*/
+
+/*CHECK(data_cadastro_pass <= now()) trigger*/
+
+/*CHECK( data_inicio <= now()),
+    CHECK( data_fim <= now()) trigger*/
--- GB_Parte1/inserts.sql
+++ GB_Parte1/inserts.sql
@@ -1,6 +1,52 @@
 insert into Condutor
-values ("Elvis","999591229", "03239158094",now()),
-("Joao","999999999", "12345678901", now()),
-("Felipe","999999999", "12345678902", now()),
-("Armando","999999999", "12345678903", now()),
-("Lucio","999999999", "12345678904", now());
+values ("12345678901", "Elvis",  "123456789", now()),
+       ("12345678902", "Joao",   "999999991", now()),
+       ("12345678903", "Felipe", "999999992", now()),
+       ("12345678904", "Armando","999999993", now()),
+       ("12345678905", "Lucio",  "999999994", now());
+
+insert into veiculo
+values ("012345678", "abc0f12", "Tesla Motors", "Roadster", 2015),
+       ("012345679", "abc0f13", "Tesla Motors", "Roadster", 2015),
+       ("012345680", "abc0f14", "Tesla Motors", "Model S", 2017),
+       ("012345681", "abc0f15", "Tesla Motors", "Model S", 2017),
+       ("012345682", "abc0f16", "Tesla Motors", "Model S", 2017),
+       ("012345683", "abc0f17", "Tesla Motors", "Model X", 2018),
+       ("012345684", "abc0f18", "Tesla Motors", "Model X", 2018),
+       ("012345685", "abc0f19", "Tesla Motors", "Model X", 2018),
+       ("012345686", "abc0f20", "Tesla Motors", "Model 3", 2019),
+       ("012345687", "abc0f21", "Tesla Motors", "Model 3", 2019),
+       ("012345688", "abc0f22", "Tesla Motors", "Model 3", 2019),
+       ("012345689", "abc0023", "Fiat", "Palio", 2015),
+       ("012345690", "abc0024", "Fiat", "Uno Sem escada", 2015),
+       ("012345691", "abd0025", "Fiat", "Uno Com Escada", 2015),
+       ("012345692", "abe0026", "Fiat", "Fiorino", 2001),
+       ("012345693", "abe0146", "Chevrolet", "Corsa", 1998),
+       ("012345694", "abe0147", "Chevrolet", "Prisma", 2012),
+       ("012345695", "abe0148", "Chevrolet", "Camaro", 2012);
+
+insert into motorista (data_inicio_mot, data_fim_mot,renavam,cpf_motorista)
+values (now(),now(),"012345678","12345678901"),
+       (now(),now(),"012345679","12345678902"),
+       (now(),now(),"012345680","12345678903"),
+       (now(),now(),"012345681","12345678904"),
+       (now(),now(),"012345682","12345678905");
+
+insert into passageiros
+values  ("Kobayashi",   "012345678", now(), "01234567890"),
+        ("Kanna",       "012345679", now(), "01234567891"),
+        ("Arthur Fleck","012345680", now(), "01234567892"),
+        ("Tooruh",      "012345681", now(), "01234567893"),
+        ("Senku",       "012345682", now(), "01234567894");
+
+insert into corrida (avaliacao_condutor
+                     ,avaliacao_veiculo
+                     ,data_inicio_corr
+                     ,data_fim_corr
+                     ,origem
+                     ,destino
+                     ,tarifa
+                     ,distancia
+                     ,id_motorista
+                     ,cpf_passageiro)
+values  (5,5,"2019-10-30",)
